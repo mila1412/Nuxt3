e8b37c6c3a2575c1842d5b64f38ce7b7eaccbc7f
milaaa <k120240305@gmail.com>
2023-12-02 00:15:52 +0800

    middleware 三種形式

---
+++ pages/user.vue
@@ -0,0 +1,20 @@
+<script setup>
+// 1. 匿名 route middleware (直接寫在頁面)
+definePageMeta({
+  middleware: (to, from) => {
+    console.log("user=>", { to, from });
+  },
+});
+
+// 2. 具名 route middleware (統一寫在 auth)
+definePageMeta({
+  middleware: ["auth"],
+});
+
+// 3. 在 middleware 檔名後面加上 global 就會變成全域的 route middleware
+</script>
+<template>
+  <div>
+    <h1>user</h1>
+  </div>
+</template>
