345af10e2158e307e3c0f17bed173b6fab89939a
milaaa <k120240305@gmail.com>
2023-12-03 01:15:25 +0800

    loading 與 404

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,28 @@
-<script setup></script>
+<script setup>
+const nuxtApp = useNuxtApp();
+const isLoading = ref(false);
+nuxtApp.hook("page:start", () => {
+  isLoading.value = true;
+});
+nuxtApp.hook("page:finish", () => {
+  isLoading.value = false;
+});
+</script>
 
 <template>
   <div>
     <p>Layout: default</p>
+    <!-- 方法一 (只能放在 layout or app.vue) -->
+    <!-- <NuxtLoadingIndicator color="#f00" :height="10" :throttle="0" /> -->
+
+    <!-- 方法二 -->
+    <div v-show="isLoading" class="loading">Loading...</div>
     <slot />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.loading {
+  color: aqua;
+}
+</style>
---
+++ pages/[...slug].vue
@@ -0,0 +1,11 @@
+<script setup>
+// 不要再 page 底下建立動態路由，不然會搶走[...slug].vue 的優先權
+const route = useRoute();
+const event = useRequestEvent();
+setResponseStatus(event, 404);
+</script>
+
+<template>
+  <h1>404 Not Found</h1>
+  <p>{{ route.params.slug }}</p>
+</template>
