182596946f222663e22b0bae26d4d8380f47ca63
milaaa <k120240305@gmail.com>
2023-12-02 23:55:27 +0800

    嵌套式路由

---
+++ pages/router.vue
@@ -0,0 +1,21 @@
+<script setup>
+const route = useRoute();
+const router = useRouter();
+
+onMounted(() => {
+  console.log(route);
+  console.log(router);
+});
+</script>
+
+<template>
+  <div>about</div>
+  <NuxtLink to="/router">index</NuxtLink>
+  <NuxtLink to="/router/a">a</NuxtLink>
+  <NuxtLink to="/router/b">b</NuxtLink>
+  <main>
+    <NuxtPage />
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
