ff39980404a99119de18c6e23e1ba7a047064356
milaaa <k120240305@gmail.com>
2023-12-03 16:25:28 +0800

    新增 pinia

---
+++ stores/vote.js
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+export const useVoteStore = defineStore("vote", () => {
+  // 是否送出投票
+  const isLoad = ref(false);
+
+  // 儲存投票列表
+  const voteList = ref({});
+
+  // 寫入 | 更新投票列表
+  const setVoteList = (list) => {
+    voteList.value = list;
+  };
+
+  // 送出投票
+  const voteSend = async (type) => {
+    if (isLoad.value) return;
+    isLoad.value = true;
+    try {
+      const res = await $fetch("https://vue-lessons-api.vercel.app/vote/add", {
+        method: "POST",
+        body: { type },
+      });
+      setVoteList(res);
+      // 先按下 alert 的確定後，isLoad.value 才會變 false
+      alert("投票完成");
+    } catch (error) {
+      alert("暫時無法投票");
+    } finally {
+      isLoad.value = false;
+    }
+  };
+
+  return { isLoad, voteList, setVoteList, voteSend };
+});
